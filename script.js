adafa70c281bf83d3aab3cee391beb429097d821
bitcodeusofware <familiaaguilaoyuela@gmail.com>
2025-10-05 14:26:26 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // Lista de canciones (puedes agregar más)
 const songs = [ 
-    { title: "ENCARA VIDA", artist: "KVRMVD", src: "ENCARA VIDA - KVRMVD.mp3",  duration: "1:28",
+    { title: "Got Us Fallin' In Love", artist: "Usher - Pitbull", src: "Usher - DJ Got Us Fallin' In Love (Official Music Video) ft. Pitbull - UsherVEVO.mp3",  duration: "4:07",
+        cover:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6eMWleZW67KoR4lgnf1duDPIWjF_3bvIukw&s",
+        background: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6eMWleZW67KoR4lgnf1duDPIWjF_3bvIukw&s",
+    }, { title: "ENCARA VIDA", artist: "KVRMVD", src: "ENCARA VIDA - KVRMVD.mp3",  duration: "1:28",
         background:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpC-mo6H-Rwy2Ubjhs7FxZuzTQlv-1u9MQ-A&s",
         cover: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpC-mo6H-Rwy2Ubjhs7FxZuzTQlv-1u9MQ-A&s",
     }, { title: "Do Ya Like x Resonance", artist: "marcella", src: "Do Ya Like x Resonance - marcella.mp3",  duration: "3:40",
